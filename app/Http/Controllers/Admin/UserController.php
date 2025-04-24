d9b01b13cb6c9226a63c9ae8047f08e4c123ff66
isam chajia <isamchajiabeat.com>
2025-04-24 09:24:04 +0100

    change admin to repository

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,43 +3,38 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Services\Admin\UserService as AdminUserService;
+use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    protected $userService;
+
+    public function __construct(AdminUserService $userService)
+    {
+        $this->userService = $userService;
+    }
 
     public function index()
     {
-
-        $users = User::paginate(5);
+        $users = $this->userService->getAllUsers();
 
         return view('admin.users.index', compact('users'));
     }
 
-
     public function show($id)
     {
-
-        $user = User::findOrFail($id);
-
+        $user = $this->userService->getUserById($id);
 
         return view('admin.users.view', compact('user'));
     }
 
     public function toggleStatus($id)
     {
-
-        $user = User::findOrFail($id);
-
-        $user->status = !$user->status;
-
-        $user->save();
+        $user = $this->userService->toggleStatus($id);
 
         return redirect()->back()->with('success', 'The status of user ' . $user->first_name . ' has been updated successfully.');
     }
-
-
-
 }
